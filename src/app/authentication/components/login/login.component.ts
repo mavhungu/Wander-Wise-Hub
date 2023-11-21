ce8388aa102372e48ad2367a4f59a74408ee9dd2
ronewa <ronewamavhungu@gmail.com>
2023-11-21 13:42:37 +0200

    adding authentication files

--- src/app/authentication/components/login/login.component.ts
+++ src/app/authentication/components/login/login.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.maxLength(6)]]
+  });
+
+  constructor(private fb:FormBuilder) {}
+
+
+  onSubmit() {
+    const loginData = this.loginForm.value;
+    console.log("Submiting ......", loginData);
+  }
 }
